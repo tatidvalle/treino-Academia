69bf5c623c98189a87f2b07e4ca90b5f0f1103b5
tatidvalle <tatidvalle88@gmail.com>
2026-02-27 10:00:44 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,24 @@
-// Cache offline básico
-const CACHE = 'treino-tatiane-v2';
+// Service Worker V3: atualização imediata
+const CACHE = 'treino-tatiane-v3';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install', (e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
-self.addEventListener('activate', (e)=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
-self.addEventListener('fetch', (e)=>{ e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request).catch(()=> caches.match('./index.html')))); });
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // ativa a nova versão sem esperar
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // assume controle das páginas abertas
+  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
